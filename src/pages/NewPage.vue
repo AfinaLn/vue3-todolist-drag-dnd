d4f1b597c68ca01c1cd292c7f0c01c4edc7e1545
AfinaLn <linasyu151@163.com>
2023-04-25 18:27:41 +0800

    主题样式修改

--- src/pages/NewPage.vue
+++ src/pages/NewPage.vue
@@ -21,7 +21,7 @@ let currentObj = reactive({})
 const dialogTitle = ref('')
 const dialogFormVisible = ref(false)
 const wide = ref(false)
-const formLabelWidth = '100px'
+const formLabelWidth = '60px'
 const repeatData = [
     {
         label: '无',
@@ -66,7 +66,7 @@ let form = ref({
 const priorityColor = {
     high: '#DB3B26',
     medium: '#FFB95B',
-    low: '#169BFA',
+    low: '#396df0',
     nomal: '#9da1aa',
 }
 const newTodo = ref('')
@@ -164,9 +164,8 @@ function addTodo() {
     ]
     newScene.children = newData
     newTodo.value = ''
-    change()
-    console.log('addtodo', newScene)
     saveData(newScene)
+    change()
 }
 function change() {
     ctx.$forceUpdate()
@@ -179,7 +178,6 @@ function openDialog(obj) {
     const { date, priority, repeat } = obj;
     form.value = { date, priority, repeat };
     dialogFormVisible.value = true;
-    console.log('==', dialogFormVisible, currentObj, form)
 }
 function resetDialog() {
     dialogTitle.value = '';
@@ -385,7 +383,7 @@ function toggleDarkOrLight() {
             <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
         </svg>
     </div>
-    <div class="app">
+    <div class="app page">
         <div class="header" :class="{ 'wide': wide }">
             <!-- <div class="menu-circle"></div>
             <div class="header-menu">
@@ -421,14 +419,14 @@ function toggleDarkOrLight() {
             <div class="main-container">
                 <div class="content-wrapper">
                     <div class="content-section">
-                        <!-- <div class="content-section-title">Apps in your plan</div> -->
-                        <div class="apps-card">
-                            <Container class="h-full flex overflow-x-auto gap-8 p-8" group-name="cols" tag="div"
+                        <!-- <div class="content-section-title">Apps in your plan</div> w-96 p-8 overflow-x-auto-->
+                        <div class="apps-card flex-row">
+                            <Container class="h-full flex  gap-8 " group-name="cols" tag="div"
                                 orientation="horizontal" @drop="onColumnDrop($event)">
                                 <Draggable
-                                    class="app-card dragg-wrapper-bg dark:bg-gray-700 rounded-lg h-full w-96 flex-shrink-0 shadow-xl"
+                                    class="app-card dragg-wrapper-bg dark:bg-gray-700 rounded-lg h-full  flex-shrink-0 shadow-xl"
                                     v-for="(column, index) in scene.children" :key="index">
-                                    <div class="h-full flex flex-col">
+                                    <div class=" h-full flex flex-col">
                                         <!-- header -->
                                         <div class="cursor-move rounded-t-lg p-4 space-x-4 flex space-x-2">
                                             <!-- class="cursor-move rounded-t-lg p-4 space-x-4 bg-primary text-white flex space-x-2"> -->
@@ -448,7 +446,7 @@ function toggleDarkOrLight() {
                                             </span>
                                         </div>
                                         <!-- column  -->
-                                        <Container class="page-content-column flex-grow overflow-y-auto overflow-x-hidden"
+                                        <Container class="page-column flex-grow overflow-y-auto overflow-x-hidden"
                                             group-name="col-items" :shouldAcceptDrop="(e, payload) => e.groupName === 'col-items'
                                                 " :get-child-payload="getCardPayload(column.type)" :drop-placeholder="{
             className: `bg-primary bg-opacity-20  
@@ -465,18 +463,46 @@ function toggleDarkOrLight() {
                                             <!-- Items  -->
                                             <template v-for="item in column.children">
                                                 <Draggable :key="item.id" :item="item" v-if="item">
-                                                    <div @click="openDialog(item)"
+                                                   <ul @click="openDialog(item)" class="cursor-move">
+                                                    <li class="list adobe-product">
+                                                    <div class="products">
+                                                        <el-icon class="el-input_icon" size="20px"
+                                                            :color="priorityColor[item.priority]">
+                                                            <HelpFilled />
+                                                        </el-icon>
+                                                       {{ item.content }}
+                                                    </div>
+                                                    <div class="button-wrapper list-right  cursor">
+                                                        <div class="list-right cursor">
+                                                            <div class="list-right-date">{{ item.dateName }}</div>
+                                                            <div class="list-right-icon" v-show="item.repeat !== 'no'">
+                                                                <el-icon size="14" class="mr5" color="#396df0">
+                                                                    <Refresh />
+                                                                </el-icon>
+                                                                <el-icon  size="14" color="#DB3B26"
+                                                                    @click.stop="submitDel(item)">
+                                                                    <Delete />
+                                                                </el-icon>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    </li>
+                                                   </ul>
+
+
+
+                                                    <!-- <div @click="openDialog(item)"
                                                         class="cursor-move my-2 mx-4 rounded-lg shadow-md dark:bg-gray-800 hover:border-2 border-primary">
-                                                        <div class="list space-y-2 flex-row space-between">
+                                                        <div class="list adobe-product space-y-2 flex-row space-between">
                                                             <div
-                                                                class="list-left rounded-lg p-2 w-max inline-block box-content flex-row">
+                                                                class="list-left products rounded-lg p-2 w-max inline-block box-content flex-row">
                                                                 <el-icon class="el-input_icon" size="20px"
                                                                     :color="priorityColor[item.priority]">
                                                                     <HelpFilled />
                                                                 </el-icon>
                                                                 <span class="list-left-name">{{ item.content }}</span>
                                                             </div>
-                                                            <div class="list-right cursor">
+                                                            <div class="button-wrapper list-right cursor">
                                                                 <div class="list-right-date">{{ item.dateName }}</div>
                                                                 <div class="list-right-icon" v-show="item.repeat !== 'no'">
                                                                     <el-icon size="14" color="#475669">
@@ -489,85 +515,25 @@ function toggleDarkOrLight() {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
+                                                    </div> -->
                                                 </Draggable>
                                             </template>
                                         </Container>
                                     </div>
                                 </Draggable>
                             </Container>
-                            <div class="app-card">
-                                <span>
-                                    <svg viewBox="0 0 512 512" style="border: 1px solid #a059a9">
-                                        <path xmlns="http://www.w3.org/2000/svg"
-                                            d="M480 0H32C14.368 0 0 14.368 0 32v448c0 17.664 14.368 32 32 32h448c17.664 0 32-14.336 32-32V32c0-17.632-14.336-32-32-32z"
-                                            fill="#210027" data-original="#7b1fa2" />
-                                        <g xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M192 64h-80c-8.832 0-16 7.168-16 16v352c0 8.832 7.168 16 16 16s16-7.168 16-16V256h64c52.928 0 96-43.072 96-96s-43.072-96-96-96zm0 160h-64V96h64c35.296 0 64 28.704 64 64s-28.704 64-64 64zM400 256h-32c-18.08 0-34.592 6.24-48 16.384V272c0-8.864-7.168-16-16-16s-16 7.136-16 16v160c0 8.832 7.168 16 16 16s16-7.168 16-16v-96c0-26.464 21.536-48 48-48h32c8.832 0 16-7.168 16-16s-7.168-16-16-16z"
-                                                fill="#f6e7fa" data-original="#e1bee7" />
-                                        </g>
-                                    </svg>
-                                    Premiere Pro
-                                </span>
-                                <div class="app-card__subtext">Edit, master and create fully proffesional videos</div>
-                                <div class="app-card-buttons">
-                                    <button class="content-button status-button">Update</button>
-                                    <div class="menu"></div>
-                                </div>
-                            </div>
-                            <div class="app-card">
-                                <span>
-                                    <svg viewBox="0 0 52 52" style="border: 1px solid #c1316d">
-                                        <g xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M40.824 52H11.176C5.003 52 0 46.997 0 40.824V11.176C0 5.003 5.003 0 11.176 0h29.649C46.997 0 52 5.003 52 11.176v29.649C52 46.997 46.997 52 40.824 52z"
-                                                fill="#2f0015" data-original="#6f2b41" />
-                                            <path
-                                                d="M18.08 39H15.2V13.72l-2.64-.08V11h5.52v28zM27.68 19.4c1.173-.507 2.593-.761 4.26-.761s3.073.374 4.22 1.12V11h2.88v28c-2.293.32-4.414.48-6.36.48-1.947 0-3.707-.4-5.28-1.2-2.08-1.066-3.12-2.92-3.12-5.561v-7.56c0-2.799 1.133-4.719 3.4-5.759zm8.48 3.12c-1.387-.746-2.907-1.119-4.56-1.119-1.574 0-2.714.406-3.42 1.22-.707.813-1.06 1.847-1.06 3.1v7.12c0 1.227.44 2.188 1.32 2.88.96.719 2.146 1.079 3.56 1.079 1.413 0 2.8-.106 4.16-.319V22.52z"
-                                                fill="#e1c1cf" data-original="#ff70bd" />
-                                        </g>
-                                    </svg>
-                                    InDesign
-                                </span>
-                                <div class="app-card__subtext">Design and publish great projects & mockups</div>
-                                <div class="app-card-buttons">
-                                    <button class="content-button status-button">Update</button>
-                                    <div class="menu"></div>
-                                </div>
-                            </div>
-                            <div class="app-card">
-                                <span>
-                                    <svg viewBox="0 0 52 52" style="border: 1px solid #C75DEB">
-                                        <g xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M40.824 52H11.176C5.003 52 0 46.997 0 40.824V11.176C0 5.003 5.003 0 11.176 0h29.649C46.997 0 52 5.003 52 11.176v29.649C52 46.997 46.997 52 40.824 52z"
-                                                fill="#3a3375" data-original="#3a3375" />
-                                            <path
-                                                d="M27.44 39H24.2l-2.76-9.04h-8.32L10.48 39H7.36l8.24-28h3.32l8.52 28zm-6.72-12l-3.48-11.36L13.88 27h6.84zM31.48 33.48c0 2.267 1.333 3.399 4 3.399 1.653 0 3.466-.546 5.44-1.64L42 37.6c-2.054 1.254-4.2 1.881-6.44 1.881-4.64 0-6.96-1.946-6.96-5.841v-8.2c0-2.16.673-3.841 2.02-5.04 1.346-1.2 3.126-1.801 5.34-1.801s3.94.594 5.18 1.78c1.24 1.187 1.86 2.834 1.86 4.94V30.8l-11.52.6v2.08zm8.6-5.24v-3.08c0-1.413-.44-2.42-1.32-3.021-.88-.6-1.907-.899-3.08-.899-1.174 0-2.167.359-2.98 1.08-.814.72-1.22 1.773-1.22 3.16v3.199l8.6-.439z"
-                                                fill="#e4d1eb" data-original="#e7adfb" />
-                                        </g>
-                                    </svg>
-                                    After Effects
-                                </span>
-                                <div class="app-card__subtext">Industry Standart motion graphics & visual effects</div>
-                                <div class="app-card-buttons">
-                                    <button class="content-button status-button">Update</button>
-                                    <div class="menu"></div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="overlay-app"></div>
-        <div class="pop-up" v-if="dialogFormVisible">
+        <div class="pop-up pop-form" v-if="dialogFormVisible">
             <div class="pop-up__title">
                 {{ dialogTitle }}
             </div>
-
-            <el-form :model="form">
+            <el-form class="mtb30" :model="form">
                 <el-form-item label="日期" :label-width="formLabelWidth">
                     <el-date-picker v-model="form.date" type="date" placeholder="选择日期" format="YYYY/MM/DD"
                         value-format="YYYY-MM-DD" />
@@ -585,7 +551,7 @@ function toggleDarkOrLight() {
                     </el-select>
                 </el-form-item>
             </el-form>
-            <div class="pop-up__subtitle">Adjust your selections for advanced options as desired before continuing. <a
+            <!-- <div class="pop-up__subtitle">Adjust your selections for advanced options as desired before continuing. <a
                     href="#">Learn more</a></div>
             <div class="checkbox-wrapper">
                 <input type="checkbox" id="check1" class="checkbox">
@@ -594,16 +560,18 @@ function toggleDarkOrLight() {
             <div class="checkbox-wrapper">
                 <input type="checkbox" id="check2" class="checkbox">
                 <label for="check2">Remove old versions</label>
-            </div>
+            </div> -->
             <div class="content-button-wrapper">
-                <button class="content-button status-button open close" @click="resetDialog">Cancel</button>
-                <button class="content-button status-button" @click="submitDialog">Continue</button>
+                <button class="content-button status-button open close" @click="resetDialog">取消</button>
+                <button class="content-button status-button" @click="submitDialog">确认</button>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+
+@import './newPage.scss';
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
 
 * {
@@ -1045,6 +1013,7 @@ body.light-mode .video-bg:before {
     height: 100%;
     overflow: auto;
     background-color: var(--theme-bg-color);
+    width: calc(100vw - 130px);
 
     @media screen and (max-width: 510px) {
         padding: 20px;
@@ -1464,7 +1433,7 @@ body.light-mode .video-bg:before {
 .app-card {
     display: flex;
     flex-direction: column;
-    width: calc(33.3% - 20px);
+    width: calc(33.3% - 40px);
     font-size: 16px;
     background-color: var(--content-bg);
     border-radius: 14px;
